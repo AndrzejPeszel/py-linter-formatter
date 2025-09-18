9c48f26ab3376ac237ef33da8c8b3ccb7ec5149b
AndrzejPeszel <andrzej.peszel@icloud.com>
2025-09-18 12:14:05 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,10 +1,26 @@
 from typing import List, Dict, Any
 
+
 def format_linter_error(error: Dict[str, Any]) -> Dict[str, Any]:
-    return {"name": error.get("code"), "line": error.get("line_number"), "column": error.get("column_number"), "message": error.get("text"), "source": "flake8"}
+    return {
+        "name": error.get("code"),
+        "line": error.get("line_number"),
+        "column": error.get("column_number"),
+        "message": error.get("text"),
+        "source": "flake8"
+    }
+
 
 def format_single_linter_file(file_path: str, errors: List[Dict[str, Any]]) -> Dict[str, Any]:
-    return {"path": file_path, "errors": [format_linter_error(error) for error in errors], "status": "passed" if not errors else "failed"}
+    return {
+        "path": file_path,
+        "errors": [format_linter_error(error) for error in errors],
+        "status": "passed" if not errors else "failed"
+    }
+
 
 def format_linter_report(linter_report: Dict[str, List[Dict[str, Any]]]) -> List[Dict[str, Any]]:
-    return [format_single_linter_file(file_path, errors) for file_path, errors in linter_report.items()]
+    return [
+        format_single_linter_file(file_path, errors)
+        for file_path, errors in linter_report.items()
+    ]
