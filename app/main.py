85b048b2e2095e5534fdfd0263ba206a45b5c741
AndrzejPeszel <andrzej.peszel@icloud.com>
2025-09-18 12:24:19 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -11,17 +11,23 @@ def format_linter_error(error: Dict[str, Any]) -> Dict[str, Any]:
     }
 
 
-def format_single_linter_file(file_path: str, errors: List[Dict[str, Any]]) -> Dict[str, Any]:
+def format_single_linter_file(
+    file_path: str,
+    errors: List[Dict[str, Any]]
+) -> Dict[str, Any]:
     return {
         "path": file_path,
         "errors": [
-            format_linter_error(error) for error in errors
+            format_linter_error(error)
+            for error in errors
         ],
         "status": "passed" if not errors else "failed",
     }
 
 
-def format_linter_report(linter_report: Dict[str, List[Dict[str, Any]]]) -> List[Dict[str, Any]]:
+def format_linter_report(
+    linter_report: Dict[str, List[Dict[str, Any]]]
+) -> List[Dict[str, Any]]:
     return [
         format_single_linter_file(file_path, errors)
         for file_path, errors in linter_report.items()
